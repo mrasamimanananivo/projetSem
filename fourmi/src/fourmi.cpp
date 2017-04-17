3caf0394f57b1f721fd939823f57e36b740f61c7
mrasamimanananivo <miaina.rasamimanananivo@insa-rouen.fr>
2017-04-17 12:17:13 +0200

    Add files via upload

---
+++ fourmi/src/fourmi.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : fourmi.cpp
+// Author      : Miaina Cynthia Rasamimanananivo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
