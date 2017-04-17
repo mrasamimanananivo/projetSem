0168a6b503843b48ebb2c0902f8ea2ced0cb09ca
CynthiaMiaina <CynthiaMiaina@cynthia-miaina>
2017-04-17 12:57:26 +0200

    system

---
+++ src/Systeme.cpp
@@ -0,0 +1,22 @@
+/*
+ * Systeme.cpp
+ *
+ *  Created on: 10 avr. 2017
+ *      Author: Clémentine
+ */
+
+#include "Systeme.h"
+
+namespace std {
+
+
+Systeme::Systeme() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Systeme::~Systeme() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ src/Systeme.hpp
@@ -0,0 +1,27 @@
+/*
+ * Systeme.h
+ *
+ *  Created on: 10 avr. 2017
+ *      Author: Clémentine
+ */
+
+#ifndef SYSTEME_H_
+#define SYSTEME_H_
+
+#include "graphe.h"
+#include "fourmi.h"
+
+
+namespace std {
+
+class Systeme {
+	vector<Fourmi> lesFourmis;
+	Graphe graphe;
+public:
+	Systeme();
+	virtual ~Systeme();
+};
+
+} /* namespace std */
+
+#endif /* SYSTEME_H_ */
